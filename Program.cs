d4c2230430f9df44441dd9ff5fcb84e52e816f0b
Carlos4415 <carlos.soares4415@gmail.com>
2025-05-15 20:28:29 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿void TestaBuscarPalavra()
+{
+    string[] arrayDePalavras = new string[5];
+
+    for (int i = 0; i < arrayDePalavras.Length; i++)
+    {
+        Console.Write($"Digite {i + 1} a palavra: ");
+        arrayDePalavras[i] = Console.ReadLine();
+    }
+
+    Console.Write("Digite a palavra a ser encontrada: ");
+    var busca = Console.ReadLine();
+
+    bool palavraEncontrada = false;
+
+    foreach (string str in arrayDePalavras)
+    {
+        if (str.Equals(busca))
+        {
+            Console.WriteLine($"Palavra encontrada = {str}");
+            palavraEncontrada = true;
+            break;
+        }
+    }
+
+    if (!palavraEncontrada)
+    {
+        Console.WriteLine($"Palavra {busca} não encontrada");
+    }
+}
+
+TestaBuscarPalavra();
